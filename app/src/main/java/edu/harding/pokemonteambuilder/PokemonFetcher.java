89d9b59f37078ff7da40e1a2ccc3fd39536f0419
Bryan Cuneo <bryan.cuneo@gmail.com>
2017-11-30 22:10:45 -0600

    Added PokeAPI wrapper

--- app/src/main/java/edu/harding/pokemonteambuilder/PokemonFetcher.java
+++ app/src/main/java/edu/harding/pokemonteambuilder/PokemonFetcher.java
@@ -1,5 +1,13 @@
 package edu.harding.pokemonteambuilder;
 
-public class PokemonFetcher {
+import me.sargunvohra.lib.pokekotlin.client.PokeApi;
+import me.sargunvohra.lib.pokekotlin.client.PokeApiClient;
+import me.sargunvohra.lib.pokekotlin.model.*;
 
+public class PokemonFetcher {
+    PokeApi mPokeApi = new PokeApiClient();
+
+    public PokemonSpecies fetchSpecies(int species) {
+        return mPokeApi.getPokemonSpecies(species);
+    }
 }
