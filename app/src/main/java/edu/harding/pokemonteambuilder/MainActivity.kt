f0d09cdeb374e176147028ce1335c1b379fb1815
Bryan Cuneo <bryan.cuneo@gmail.com>
2017-12-04 17:02:07 -0600

    Some more API testing

--- app/src/main/java/edu/harding/pokemonteambuilder/MainActivity.kt
+++ app/src/main/java/edu/harding/pokemonteambuilder/MainActivity.kt
@@ -2,9 +2,18 @@ package edu.harding.pokemonteambuilder
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Debug
+import android.util.Log
 import android.view.View
+import me.sargunvohra.lib.pokekotlin.client.PokeApi
+import me.sargunvohra.lib.pokekotlin.client.PokeApiClient
+import me.sargunvohra.lib.pokekotlin.model.Pokemon
+import org.jetbrains.anko.doAsync
+import java.io.Console
 
 class MainActivity : AppCompatActivity() {
+    //val api: PokemonFetcher = PokemonFetcher()
+    val mPokeApi: PokeApi = PokeApiClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +24,15 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
+    fun buildTrainerCard(view: View) {
+        doAsync {
+            Log.d("API", "starting...")
+            var p: Pokemon = mPokeApi.getPokemon(1)
+            Log.d("BULB", p.name)
+            Log.d("BULB", p.types.toString())
+            Log.d("BULB", p.moves.toString())
+            Log.d("BULB", p.abilities.toString())
+            Log.d("API", "done")
+        }
+    }
 }
--- app/src/main/java/edu/harding/pokemonteambuilder/PokemonFetcher.kt
+++ app/src/main/java/edu/harding/pokemonteambuilder/PokemonFetcher.kt
@@ -1,13 +1,20 @@
 package edu.harding.pokemonteambuilder
 
+import android.support.compat.R.id.async
 import me.sargunvohra.lib.pokekotlin.client.PokeApi
 import me.sargunvohra.lib.pokekotlin.client.PokeApiClient
 import me.sargunvohra.lib.pokekotlin.model.*
+import org.jetbrains.anko.doAsync
 
 class PokemonFetcher {
-    internal var mPokeApi: PokeApi = PokeApiClient()
+    private var mPokeApi: PokeApi = PokeApiClient()
 
-    fun fetchSpecies(species: Int): PokemonSpecies {
-        return mPokeApi.getPokemonSpecies(species)
+    fun fetchSpecies(pokedexIndex: Int): PokemonSpecies {
+        return mPokeApi.getPokemonSpecies(pokedexIndex)
+    }
+
+    fun speciesToPokemon(species: PokemonSpecies): Pokemon {
+        var pokemon = Pokemon()
+        return pokemon
     }
 }
