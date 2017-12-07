fb52c5455e9d100bb46da3ebd63071746fc2123f
Bryan Cuneo <bryan.cuneo@gmail.com>
2017-12-07 14:17:02 -0600

    Added some more ActionBars
    Fixed another resource name

--- app/src/main/java/edu/harding/pokemonteambuilder/TrainerCardDoneActivity.kt
+++ app/src/main/java/edu/harding/pokemonteambuilder/TrainerCardDoneActivity.kt
@@ -3,17 +3,24 @@ package edu.harding.pokemonteambuilder
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 import android.view.View
 
 class TrainerCardDoneActivity : AppCompatActivity() {
 
-    var mTeam: PokemonTeam 
+    var mTeam: PokemonTeam = PokemonTeam()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trainer_card_done)
 
+        setupActionBar()
+    }
 
+    private fun setupActionBar() {
+        var ab: Toolbar = findViewById(R.id.toolbar)
+        ab.title = "Trainer Card Complete"
+        setSupportActionBar(ab)
     }
 
     fun backToMainActivity(view: View){
