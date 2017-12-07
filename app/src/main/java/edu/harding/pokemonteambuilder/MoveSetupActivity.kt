9136f966bda277efbc9569d873d584ff7c3ca8e7
zackeezy <zacaddison25@gmail.com>
2017-12-06 19:24:39 -0600

    Adding activity for moves

---
+++ app/src/main/java/edu/harding/pokemonteambuilder/MoveSetupActivity.kt
@@ -0,0 +1,14 @@
+package edu.harding.pokemonteambuilder
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveSetupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_setup)
+    }
+
+
+}
